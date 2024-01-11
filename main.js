1c3cdf6039a17e3dee897f7c4b8cfe91ce44139e
nehal <nehalr@seas.upenn.edu>
2024-01-11 10:08:19 -0600

    added stuff

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function() {
     createBoard(16);
-    console.log("Howdy")
+    let btn = document.querySelector("#popup");
+    btn.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size)
+    })
+    console.log("Hi Jill!")
 
 });
 
@@ -14,7 +21,38 @@ function createBoard(size) {
 
     for (let i = 0; i < numDives; i++) {
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow"
+        div.addEventListener("mouseover", colorDiv);
         board.insertAdjacentElement("beforeend", div);
     }
+}
+
+function getSize() {
+    let input = prompt("Give me a size for the board!")
+    let message  = document.querySelector("#message");
+    if (input === ""){
+        message.innerHTML = "Please provide a number";
+    } else if (input < 0 || input > 100) {
+        message.innerHTML = "Please stay in the bounds of 0 through 100";
+    } else {
+        message.innerHTML = "Woohoo, now you can play!";
+        return input;
+    }
+}
+
+function colorDiv(){
+    if (color == "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    } else {
+        this.style.backgroundColor = "black";
+    }
+    
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor = "white");
 }
